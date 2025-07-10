262cae6e1625d840fbb3d66f0b7645cc811de331
Andrew Davis <jonandrewdavis@gmail.com>
2025-07-09 22:33:22 -0400

    chore: all of our dependencies needed to be added back

--- typescript_server/base/constants.ts
+++ typescript_server/base/constants.ts
@@ -1,6 +1,5 @@
-import { env } from "process";
 import { secretKey } from "../configuration.json";
 
 export class Constants {
-  public static SecretKey: String = env.SECRET_KEY || secretKey;
+  public static SecretKey: String = process.env.USER_ID || secretKey;
 }
